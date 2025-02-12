ed510cc257cab23c482b1e2a15ed8aa60b2e675f
KidmanC <127449647+KidmanC@users.noreply.github.com>
2025-02-11 21:56:54 -0500

    l

--- run-benchmarks.sh
+++ run-benchmarks.sh
@@ -17,31 +17,12 @@ for lang in "${languages[@]}"; do
 
     # Construir la imagen de Docker
     echo "Construyendo imagen para $lang..."
-    docker build -t sum_primes_$lang . > /dev/null
+    docker build -t sum_primes_$lang .
 
     # Ejecutar el contenedor y capturar el tiempo de ejecución
     echo "Ejecutando contenedor para $lang..."
-    output=$(docker run --name sum_primes_${lang}_container sum_primes_$lang)
-    execution_time=$output
-
-    # Copiar el archivo de salida desde el contenedor
-    docker cp sum_primes_${lang}_container:/app/$lang/${lang}_output.txt ../outputs/${lang}_output.txt > /dev/null
-
-    # Eliminar el contenedor
-    docker rm sum_primes_${lang}_container > /dev/null
-
-    # Almacenar el tiempo de ejecución y resultado
-    times[$lang]=$execution_time
-    results[$lang]=$(cat ../outputs/output_${lang}.txt)
-
-    # Volver al directorio anterior
+    docker run --rm -v "$(pwd)/$lang:app/$lang" sum_primes_$lang
     cd ..
 done
 
-# Mostrar la tabla de resultados
-echo -e "\nResultados del Benchmark:"
-printf "%-10s %-25s %-15s\n" "Lenguaje" "Tiempo de Ejecución (ms)" "Resultado"
-for lang in "${languages[@]}"; do
-    printf "%-10s %-25s %-15s\n" "$lang" "${times[$lang]}" "${results[$lang]}"
-done
 
